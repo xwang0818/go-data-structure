d1ef0e66f29e356d66af058642235dbed7959c31
Xiang Wang <xwang1314@gmail.com>
2020-07-23 13:08:36 -0500

    Add Copyright

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -1,3 +1,9 @@
+/* Copyright (C) Xiang Wang - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Xiang Wang <xwang1314@gmail.com>, July 2020
+ */
+
 package tree
 
 import "testing"
